acf0399f4d70e72f51a9190d75210c915f2649a8
Himanshu Mishra <himanshu2014iit@gmail.com>
2016-12-27 22:22:06 +0530

    Use variable to decide making comment

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def redirected(stdout):
 @app.route("/", methods=['GET', 'POST'])
 def main():
     if request.method == "POST" and "action" in request.json:
+        PERMITTED_TO_COMMENT = True
         if request.json["action"] in ["synchronize", "opened"]:
             after_commit_hash = request.json["pull_request"]["head"]["sha"]
             repository = request.json["repository"]["full_name"]
@@ -67,7 +68,7 @@ def main():
 
 
 
-            # Make the comment
+            # Write the comment body
             if request.json["action"] == "opened":
                 comment = "Hello @" + author + "! Thanks for submitting the PR.\n\n"
             elif request.json["action"] == "synchronize":
@@ -96,11 +97,11 @@ def main():
                 if old_comment["user"]["id"] == 24736507:  # ID of @pep8speaks
                     last_comment = old_comment["body"]
                     break
-            print("Comment is \n")
-            print(comment)
-            print("Last comment is \n")
-            print(last_comment)
-            if comment != last_comment:
+            if comment == last_comment:
+                PERMITTED_TO_COMMENT = False
+
+            # Make the comment
+            if PERMITTED_TO_COMMENT:
                 query = "https://api.github.com/repos/" + repository + "/issues/" + \
                         str(pr_number) + "/comments?access_token={}".format(
                             os.environ["GITHUB_TOKEN"])
@@ -112,58 +113,6 @@ def main():
     else:
         return render_template('index.html')
 
-
-"""
-# @app.route("/student-register", methods=['GET', 'POST'])
-def student_register(request):
-    flag = None
-    global conn, cursor
-    if "LOCAL_CHECK" not in os.environ:
-        msg = "Database Connection cannot be set since you are running website locally"
-        msgcode = 0
-        return {"web": 'index.html' , "flag":"True", "msg":msg,"msgcode":msgcode}
-
-    if request.method == "POST":
-        form_dict = request.form.to_dict()
-        query = r"INSERT INTO student (f_name,l_name,email_id,roll_no,git_handle) values ('%s','%s','%s','%s','%s') " % (
-            form_dict["fname"], form_dict["lname"], form_dict["emailid"], form_dict["rollno"], form_dict["githubhandle"])
-
-        try:
-            cursor.execute(query)
-            conn.commit()
-            mail_subject = "Successfully registered for Kharagpur Winter of Code!"
-            #mail_body = 'Hello ' + form_dict["fname"] + '<br>You have been successfully registered for the <b>Kharagpur Winter of Code</b>. ' + \
-            #            'Check out the <a href="http://kwoc.kossiitkgp.in/resources">Resources for KWoC</a> now.'
-
-            mail_body = mail_body.format(form_dict['fname'])
-            mail_check = send_mail(
-                mail_subject, mail_body, form_dict["emailid"])
-            if not mail_check:
-                slack_notification("Unable to send mail to the following student :\n{}".format(
-                    form_dict))
-            flag="True"
-            msg=form_dict["fname"] + ", You have been successfully registered. Please check your email for instructions."
-            msgcode=1
-            return {"web": 'index.html' , "flag":flag, "msg":msg,"msgcode":msgcode}
-        except psycopg2.IntegrityError:
-            conn.rollback()
-            error_msg = "{}\n\nForm : {}".format(
-                traceback.format_exc(), form_dict)
-            slack_notification(error_msg)
-            flag="True"
-            msg="Registration Failed ! User already registered"
-            msgcode=0
-            return {"web": 'index.html' , "flag":flag, "msg":msg,"msgcode":msgcode}
-        except:
-            conn.rollback()
-            error_msg = "{}\n\nForm : {}".format(
-                traceback.format_exc(), form_dict)
-            slack_notification(error_msg)
-            flag="True"
-            msg="Registration Failed ! Please try again."
-            msgcode=0
-            return {"web": 'index.html' , "flag":flag, "msg":msg,"msgcode":msgcode}
-"""
 
 app.secret_key = os.environ["APP_SECRET_KEY"]
 app.config['SESSION_TYPE'] = 'filesystem'
