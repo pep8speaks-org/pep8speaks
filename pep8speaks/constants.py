0e306b7d35f097c50b467f25563b19707222a77e
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-07-19 11:16:47 +0530

    Add constants

---
+++ pep8speaks/constants.py
@@ -0,0 +1,4 @@
+# HEADERS is deprecated, use AUTH only
+HEADERS = {"Authorization": "token " + os.environ["GITHUB_TOKEN"]}
+AUTH = (os.environ["BOT_USERNAME"], os.environ["BOT_PASSWORD"])
+BASE_URL = 'https://api.github.com'
