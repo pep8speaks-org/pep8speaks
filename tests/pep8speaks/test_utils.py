0a6ed656ad8df693c127a359a9071d3821b6d476
chinskiy <chinskiy93@gmail.com>
2017-10-07 20:41:42 +0300

    add test for utils.update_dict

--- tests/pep8speaks/test_utils.py
+++ tests/pep8speaks/test_utils.py
@@ -1,1 +1,22 @@
-# TODO
+import pytest
+from pep8speaks.utils import update_dict
+
+
+class TestUtils:
+
+    @pytest.mark.parametrize('base, head, expected', [
+        ({}, {}, {}),
+        ({}, {"k1": "v1"}, {}),
+        ({"k1": "v1"}, {}, {"k1": "v1"}),
+        ({"k1": "v1"}, {"k1": "v2"}, {"k1": "v2"}),
+        ({"k1": "v1"}, {"k1": None}, {"k1": None}),
+        ({"k1": "v1"}, {"k2": "v2"}, {"k1": "v1"}),
+        ({"k1": ["v1", "v2", "v3"]}, {"k1": "v1"}, {"k1": "v1"}),
+        ({"k1": "v1"}, {"k1": {"k2": {'k3': 3}}}, {"k1": "v1"}),
+        ({"k1": "v1"}, {"k1": ["v1", "v2", "v3"]}, {"k1": ["v1", "v2", "v3"]}),
+        ({"k1": {"k2": "v1"}}, {"k1": "v1"}, {"k1": "v1"}),
+        ({"k1": {"k2": "v1"}}, {"k1": {"k2": "v2"}}, {"k1": {"k2": "v2"}}),
+        ({"k1": {"k2": "v1", "k3": "v2"}}, {"k1": {"k2": "v3"}}, {'k1': {'k2': 'v3', 'k3': 'v2'}}),
+    ])
+    def test_update_dict(self, base, head, expected):
+        assert update_dict(base, head) == expected
