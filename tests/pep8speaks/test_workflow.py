2825c0a0af2fe0277d018671eadbb1c40f0535f7
Himanshu Mishra <himanshu@orkohunter.net>
2019-02-05 15:33:44 +0530

    WIP

---
+++ tests/pep8speaks/test_workflow.py
@@ -0,0 +1,21 @@
+"""
+[ ] Create a new PR involving Python files with errors without .pep8speaks.yml
+[ ] Create a new PR involving Python files with errors with .pep8speaks.yml
+[ ] Create a new PR involving Python files without errors without .pep8speaks.yml
+[ ] Create a new PR involving Python files without errors with .pep8speaks.yml
+[ ] Create a new PR not involving Python files with errors
+[ ] Add a new commit to an opened PR with fixing the issues with .pep8speaks.yml
+[ ] Add a new commit to an opened PR with fixing the issues without .pep8speaks.yml
+[ ] Add a new commit to an opened PR with maintaining the issues with .pep8speaks.yml
+[ ] Add a new commit to an opened PR with maintaining the issues without .pep8speaks.yml
+[ ] Add a new commit to an opened PR with creating new issues with .pep8speaks.yml
+[ ] Add a new commit to an opened PR with creating new issues without .pep8speaks.yml
+"""
+
+class TestFlow1(object):
+
+    def setUp(self):
+        pass
+
+    def test_one(self):
+        assert 1 == 1
